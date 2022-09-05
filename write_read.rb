710d96aba7350b9901b22e97135c5f9f9f4ea66a
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-05 23:46:05 +0300

    Add read and write methods

---
+++ write_read.rb
@@ -0,0 +1,31 @@
+require_relative './game'
+
+def write_games_data
+  games = []
+  @games.each do |game|
+    @player = {
+      'first_name' => game.author.first_name,
+      'last_name' => game.author.last_name,
+      'multiplayer' => game.multiplayer,
+      'last_played_at' => game.last_played_at,
+      'puhlish_date' => game.puhlish_date,
+      'archived' => game.archived
+    }
+    games << @player
+  end
+
+  FileUtils.mkdir_p('storage')
+  File.write('./storage/games.json', JSON.preety_generate(games), mode: 'w')
+end
+
+def read_games_data
+  return unless File.exist?('./storage/games.json')
+
+  games = File.read('./storage/games.json')
+  games = JSON.parse(games)
+  games.each do |play|
+    new_game = Game.new(play['multiplayer'], play['last_played_at'], play['publish_date'], play['archived'])
+    new_game.author = Author.new(play['first_name'], play['last_name'])
+    @games << new_game
+  end
+end
