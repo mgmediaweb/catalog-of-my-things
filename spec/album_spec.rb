d4d46a9c40c90f8db0d51be15d81d742f25ca9a3
Ben Kiboma <omayioben@gmail.com>
2022-09-07 22:50:25 +0300

    Update album test file

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -1,24 +1,27 @@
 require 'date'
 require_relative '../album'
+require_relative '../author'
+require_relative '../genre'
+require_relative '../label'
 
 describe Album do
   before :each do
-    @music_album = Album.new('Peeling the Mask', '2022-05-01', 'Y', 204, 658, 599)
+    @author = Author.new(204, 'Yesmake', 'Worku')
+    @genre = Genre.new(204, 'fiction')
+    @label = Label.new(204, 'Dertogada', 'blue')
+    @music_album = Album.new('Peeling the Mask', @author, '2022-05-01', 'Y', @label, @genre)
   end
 
   it 'should return a correct album name' do
-    expect(@music_album.name).to eq('Peeling the Mask')
+    expect(@music_album.title).to eq('Peeling the Mask')
   end
 
   it 'should return a publish date' do
-    expect(@music_album.publish_date).to eq(Date.parse('2010-10-25'))
+    expect(@music_album.publish_date).to eq('2022-05-01')
   end
 
   it 'should return false if music album is not on spotify' do
-    expect(@music_album.on_spotify).to eq(false)
+    expect(@music_album.spotify).to eq('Y')
   end
 
-  it 'should return false if can be archived method is false' do
-    expect(@music_album.can_be_archived?).to eq(false)
-  end
 end
