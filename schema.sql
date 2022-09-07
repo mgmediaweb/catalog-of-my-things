3127c0fbb6df23563d853e0a473864f1f68b53e3
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-07 19:40:44 +0300

    Add tests

--- schema.sql
+++ schema.sql
@@ -15,3 +15,37 @@ CREATE TABLE authors (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL
 );
+
+
+-- Creating books table
+
+CREATE TABLE books (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publisher VARCHAR(150) NOT NULL,
+    cover_state VARCHAR(150) NOT NULL,
+    publish_date DATE NOT NULL
+);
+
+-- Creating lable tables 
+
+CREATE TABLE labels (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(150) NOT NULL,
+    color VARCHAR(150) NOT NULL
+);
+
+
+-- Creating music album table  
+
+CREATE TABLE music_albums (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    on_spotify BOOLEAN NOT NULL,
+    publish_date DATE NOT NULL
+); 
+
+-- Creating genre table
+
+CREATE TABLE geners (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(150) NOT NULL
+); 
---
+++ spec/book_spec.rb
@@ -0,0 +1,14 @@
+require_relative './book' 
+
+describe Book do 
+  before(:each) do 
+    @book = Book.new('The sign and the seal', '2022-09-06')
+
+  end
+
+  context "#initialize" do 
+    it '' do 
+      
+    end
+  end
+end
