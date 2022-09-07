37e184bfffe9f1813c90e307b2b674764f5e6746
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-07 23:13:55 +0300

    Make test on game file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,20 +8,19 @@ describe Game do
     @author = Author.new(204, 'Addisu', 'Haile')
     @genre = Genre.new(204, 'War')
     @label = Label.new(204, 'Best sellers', 'Red')
-    @game1 = Game.new('Domino', 'Y', '2013-05-09', 'BirhanPlc', 'good', @author, @label, @genre)
-    @game2 = Game.new('Cheese', 'N', '2011-05-11', 'WillyPlc', 'bad', @author, @label, @genre)
+    @game = Game.new('Domino', 'Y', '2013-05-09', '2014-02-08', @author, @genre, @label)
   end
 
   it 'Crate a new instance of Game' do
-    expect(@game1).to be_a(Game)
+    expect(@game.title).to eq('Domino')
   end
 
-  # it 'Returns properties with getter' do
-  #   expect(@game1.multiplayer).to eq 'Y'
-  #   expect(@game1.last_played_at).to eq '2021-09-04'
-  # end
+  it 'Returns properties with getter' do
+    expect(@game.multiplayer).to eq 'Y'
+    expect(@game.last_played_at).to eq '2013-05-09'
+  end
 
-  # it 'can_be_achieved? return false if @last_played_at is < 2' do
-  #   expect(@game1.can_be_achieved?).to eq false
-  # end
+  it 'can_be_achieved? return false if @last_played_at is < 2' do
+    expert(@game.archived).to eq false
+  end
 end
